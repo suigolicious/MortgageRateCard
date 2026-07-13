ae16b59abc774421cf15f1b46f01854a88b828b5
Nathaniel Montalvo <nathanielmontalvo@Mac.attlocal.net>
2026-07-13 17:37:38 -0500

    this is my first commit

---
+++ src/Components/RateCard.tsx
@@ -0,0 +1,11 @@
+import './RateCard.scss';
+
+export default function RateCard() {
+  return (
+    <div className="rate-card">
+      <div className="rate-card-outline">
+        Today's Mortage Rate
+      </div>
+    </div>
+  )
+}
