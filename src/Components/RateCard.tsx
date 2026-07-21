95819b41985e73d20a3e6e8fe751e1d3422cea14
Nathaniel Montalvo <nathanielmontalvo@Nathaniels-MacBook-Pro.local>
2026-07-21 14:30:57 -0500

    my intial commit

---
+++ src/Components/RateCard.tsx
@@ -0,0 +1,11 @@
+import './RateCard.scss'
+
+export default function RateCard(){
+  return(
+    <div className="rate-card">
+      <div className="rate-card-header">
+
+      </div>
+    </div>
+  )
+}
