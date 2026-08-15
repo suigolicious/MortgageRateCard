938c868a3aab917ada9ed4b92da88d4f6a7357c7
Nathaniel4832 <nmontalvo5120@gmail.com>
2026-08-14 20:27:31 -0500

    header added

--- src/Components/RateCard.tsx
+++ src/Components/RateCard.tsx
@@ -4,7 +4,11 @@ export default function RateCard() {
   return (
     <div className="rate-card">
       <div className="rate-card-outline">
-        Today's Mortage Rate
+        <div className="title-subtitle">
+          <div className="title">Today's Mortage Rate</div>
+          <div>Updated April 12, 2026 &bull; National Averages  </div>
+        </div>
+        <div className="years">30-yr fixed</div>
       </div>
     </div>
   )
